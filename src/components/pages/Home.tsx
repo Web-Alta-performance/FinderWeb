f3602f4212d902763fc665fc4305dbe9df8c08fd
MarcioSobel <marcinhosobel@hotmail.com>
2024-03-28 09:52:46 -0300

    Very important information

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 const Home = () => {
+    const img_link = 'https://ih1.redbubble.net/image.4309935717.4355/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg';
     return (
-        <p>This is the homepage!</p>
+        <>
+        <h4>This is the homepage!</h4>
+        <img alt="gato joinha" src={img_link} />
+        </>
     )
 };
 
