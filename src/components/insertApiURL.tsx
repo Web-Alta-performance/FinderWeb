b371e20382e449f4e84ad2822e6657973402397e
marciosobel <marcinhosobel@hotmail.com>
2024-04-09 09:55:48 -0300

    add api link validation

--- src/components/insertApiURL.tsx
+++ src/components/insertApiURL.tsx
@@ -1,26 +1,43 @@
+import axios from 'axios';
 import '../css/main.css';
 import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { LoadingIcon } from './loading-icon/LoadingIcon';
 
 interface insertApiLinkProps {
     setFunc: Dispatch<SetStateAction<string>>
 }
 
-const checkIfApiExists = async () => {
-    // TODO: check if api exists
-    return false;
-}
-
 const InsertApiURL = ({ setFunc }: insertApiLinkProps) => {
+
+    const checkIfApiExists = async (baseURL: string) => {
+
+        setLoading(true);
+        return await axios.get(`/`, { baseURL })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log(response)
+                    setLoading(false);
+                    return true
+                }
+                return false
+            })
+            .catch((error) => {
+                console.error(error)
+                setLoading(false);
+                return false;
+            });
+    }
 
     const [apiExists, setApiExists] = useState(false);
     const [emptyInput, setEmptyInput] = useState(true);
+    const [loading, setLoading] = useState(false);
     const inputRef = useRef<HTMLInputElement>(null);
 
     const handleClick = async () => {
         if (inputRef.current) {
             if (inputRef.current.value) {
                 setEmptyInput(false);
-                const exists = await checkIfApiExists()
+                const exists = await checkIfApiExists(inputRef.current.value)
                 setApiExists(exists)
             }
         }
@@ -38,12 +55,21 @@ const InsertApiURL = ({ setFunc }: insertApiLinkProps) => {
             Link inválido. Verifique se o link está correto e tente novamente.
         </p>
 
+    const button = loading
+        ? <LoadingIcon />
+        : <button
+            className='text center bold size-115 button'
+            style={{ paddingInline: 20, paddingBlock: 10, borderRadius: 10, marginTop: 10, }}
+            onClick={handleClick}>
+            Confirmar
+        </button>
+
     return (
         <div className='flex center direction-column' style={{ gap: 5 }}>
             <p className='text center bold size-125'>Insira a URL da api:</p>
             {apiDoesntExistWarning}
             <input className='flex center input' style={{ padding: 10, borderRadius: 7, }} ref={inputRef} />
-            <button className='text center bold size-115 button' style={{ paddingInline: 20, paddingBlock: 10, borderRadius: 10, marginTop: 10, }} onClick={handleClick}>Confirmar</button>
+            {button}
         </div >
     );
 };
