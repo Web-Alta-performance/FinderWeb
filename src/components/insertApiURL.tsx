b698b592bc9ef20a3b04c4f14403b348f2c5829b
marciosobel <marcinhosobel@hotmail.com>
2024-04-09 04:09:11 -0300

    insert api prompt

---
+++ src/components/insertApiURL.tsx
@@ -0,0 +1,53 @@
+import '../css/main.css';
+import styles from '../css/insertApiURL.module.css';
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+
+interface insertApiLinkProps {
+    setFunc: Dispatch<SetStateAction<string>>
+}
+
+const checkIfApiExists = async () => {
+    // TODO: check if api exists
+    return false;
+}
+
+const InsertApiURL = ({ setFunc }: insertApiLinkProps) => {
+
+    const [apiExists, setApiExists] = useState(false);
+    const [emptyInput, setEmptyInput] = useState(true);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleClick = async () => {
+        if (inputRef.current) {
+            if (inputRef.current.value) {
+                setEmptyInput(false);
+                const exists = await checkIfApiExists()
+                setApiExists(exists)
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (apiExists && inputRef.current) {
+            setFunc(inputRef.current.value);
+        }
+        console.log(apiExists)
+    }, [apiExists, setFunc])
+
+    const apiDoesntExistWarning = !apiExists && emptyInput
+        ? <></>
+        : <p className={`text center warning bold size-115 ${styles.warning}`} style={{ margin: 0, }}>
+            Link inválido. Verifique se o link está correto e tente novamente.
+        </p>
+
+    return (
+        <div className='flex center direction-column' style={{ gap: 5 }}>
+            <p className='text center bold size-125'>Insira a URL da api:</p>
+            {apiDoesntExistWarning}
+            <input className={`flex center ${styles.input}`} style={{ padding: 10, borderRadius: 7, }} ref={inputRef} />
+            <button className={`text center bold size-115 ${styles.button}`} style={{ paddingInline: 20, paddingBlock: 10, borderRadius: 10, marginTop: 10, }} onClick={handleClick}>Confirmar</button>
+        </div >
+    );
+};
+
+export { InsertApiURL };
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,21 @@
 import '../../css/main.css';
+import { useState } from 'react';
+import { InsertApiURL } from '../insertApiURL';
 
 const Home = () => {
+
+    const [apiURL, setApiURL] = useState('');
+
+    const isApiURLSet = () => !!apiURL
+
+    const insertApiPrompt = isApiURLSet()
+        ? <p>Sim: {apiURL}</p>
+        : <InsertApiURL setFunc={setApiURL} />
+
     return (
         <>
             <h1 className="text center title">Página de administração PetFinder</h1>
+            {insertApiPrompt}
         </>
     )
 };
