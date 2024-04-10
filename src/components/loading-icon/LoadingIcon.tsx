ac642ad63ab0575dcf5b7bfe0096a133e1671c4e
marciosobel <marcinhosobel@hotmail.com>
2024-04-10 00:05:07 -0300

    update LoadingIcon to be able to show text

--- src/components/loading-icon/LoadingIcon.tsx
+++ src/components/loading-icon/LoadingIcon.tsx
@@ -3,11 +3,18 @@ import loadingIcon from './loading.svg'
 import styles from './loading.module.css';
 
 const LoadingIcon = ({
-    width = 50
+    width = 50,
+    showText = false,
+    text = '',
 }) => {
+    const textComponent = showText
+        ? <p className='text center size-115' style={{ margin: 0 }}>{text ? text : 'Carregando...'}</p>
+        : <></>
+
     return (
-        <div className='flex center'>
+        <div className='flex center column'>
             <img className={styles.loading} alt='loading' src={loadingIcon} style={{ width, margin: 20 }} />
+            {textComponent}
         </div>
     )
 }
