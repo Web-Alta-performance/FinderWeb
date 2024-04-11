037c8cf53c591724522e311d39d2c51dedf23a79
marciosobel <marcinhosobel@hotmail.com>
2024-04-10 23:59:00 -0300

    add loading and update button to user collections

--- src/components/petfinder_collections/userCollection.tsx
+++ src/components/petfinder_collections/userCollection.tsx
@@ -1,5 +1,56 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useContext, useState } from "react";
+import { ApiURLContext } from "../pages/Home";
+import { LoadingIcon } from "../loading-icon/LoadingIcon";
+
+interface User {
+    name: string;
+    email: string;
+    password_hash: string;
+}
+
 const UserCollection = () => {
-    return <p>User collection </p>
+
+    const baseURL = useContext(ApiURLContext);
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    const fetch = async () => {
+        setLoading(true);
+        const response = await axios.get('/users', { baseURL });
+        console.log(response.data.users);
+        setUsers(response.data.users)
+    }
+
+    const updateButton =
+        <button
+            className='text center bold size-115 button'
+            style={{ padding: '10px 20px', borderRadius: 12, marginBlock: 20 }}
+            onClick={fetch}>
+            Atualizar
+        </button >;
+
+    if (loading) {
+        return (
+            <div className='flex center column' style={{ gap: 10 }}>
+                {updateButton}
+                <LoadingIcon showText />
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            {updateButton}
+            {users.map((user, i) => (
+                <div key={i}>
+                    <p>Name: {user.name}</p>
+                    <p>Email: {user.email}</p>
+                </div>
+            ))}
+        </div>
+    )
 };
 
 export { UserCollection };
