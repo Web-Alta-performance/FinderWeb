dda2ba2d2d69a1eefa650c1eed67116ab2ba0529
marciosobel <marcinhosobel@hotmail.com>
2024-04-11 01:24:48 -0300

    shh

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -118,10 +118,7 @@ const About = () => {
         <div className='flex flex1 center' style={{ marginTop: 100, marginBottom: 20 }}>
             <div className='flex column center'>
                 <img alt='amonguinho' src={amonguinho} width={50} />
-                <div className='flex center' style={{ gap: 10 }}>
-                    <p>marci esteve aqui</p>
-                    <img alt="gif" src="https://cdn3.emoji.gg/emojis/9662-catclap.gif" width={20} height={20} />
-                </div>
+                <p>marci esteve aqui</p>
             </div>
         </div>
     </div>
