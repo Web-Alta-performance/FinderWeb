8443705ba373cd25fedc43edb305f3d6793ae68c
marciosobel <marcinhosobel@hotmail.com>
2024-04-11 13:01:04 -0300

    Add create user section

--- src/components/petfinder_collections/userCollection.tsx
+++ src/components/petfinder_collections/userCollection.tsx
@@ -1,33 +1,37 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import axios from "axios";
-import { useContext, useState } from "react";
+import axios, { AxiosError } from "axios";
+import React, { forwardRef, useContext, useEffect, useRef, useState } from "react";
 import { ApiURLContext } from "../pages/Home";
 import { LoadingIcon } from "../loading-icon/LoadingIcon";
 
 interface User {
     id: string;
-    phone_number: string,
     created_at: string;
     name: string;
     email: string;
     password_hash: string;
 }
 
-const UserTable = ({ users }: { users: User[] }) => {
-    // TODO: add a CREATE, UPDATE, and DELETE buttons.
+interface UserCreateFields {
+    name: string,
+    email: string,
+    password: string,
+}
+
+// TODO: add an UPDATE and a DELETE buttons.
+const UserList = ({ users }: { users: User[] }) => {
     if (users.length === 0) {
         return <p className='text center size-115'>Nenhum usuário encontrado.</p>;
     }
 
     const columns = (
-        <div className='flex-1 center' style={{ marginTop: 20 }}>
+        <div className='flex center column gap-15' style={{ marginBlock: 20 }}>
             {users.map((user, i) => {
-                const GAP = 10;
                 const KEY = i + 1;
                 return (
                     <div
                         key={KEY}
-                        className='flex-1 flex-start column'
+                        className='flex-1 column'
                         style={{
                             backgroundColor: 'var(--dark-background)',
                             paddingInline: 14,
@@ -37,19 +41,16 @@ const UserTable = ({ users }: { users: User[] }) => {
                         }}
                     >
                         <div>
-                            <p key={KEY * 2} className='text' style={{ margin: GAP }}>
+                            <p key={KEY * 2} className='text gap-10'>
                                 <span className='text bold'>ID: </span>{user.id}
                             </p>
-                            <p key={KEY * 3} className='text' style={{ margin: GAP }}>
+                            <p key={KEY * 3} className='text gap-10'>
                                 <span className='text bold'>Nome: </span>{user.name}
                             </p>
-                            <p key={KEY * 4} className='text' style={{ margin: GAP }}>
+                            <p key={KEY * 4} className='text gap-10'>
                                 <span className='text bold'>E-mail: </span>{user.email}
                             </p>
-                            <p key={KEY * 5} className='text' style={{ margin: GAP }}>
-                                <span className='text bold'>Telefone: </span>{user.phone_number ?? 'Indisponível'}
-                            </p>
-                            <p key={KEY * 6} className='text' style={{ margin: GAP }}>
+                            <p key={KEY * 5} className='text gap-10'>
                                 <span className='text bold'>Criado em: </span>{new Date(user.created_at).toDateString()}
                             </p>
                         </div>
@@ -66,6 +67,136 @@ const UserTable = ({ users }: { users: User[] }) => {
     );
 };
 
+const InputField = forwardRef<HTMLInputElement, { children: React.ReactNode }>(({
+    children
+}, ref) => {
+    return (
+        <div className='flex space-between gap-20'>
+            <p className='text gap-10'>
+                <span className='text bold'>
+                    {children}
+                </span>
+            </p>
+            <input
+                ref={ref}
+                className='input flex'
+                style={{
+                    marginBlock: 7,
+                    padding: '5px 7px',
+                    backgroundColor: 'var(--background-color)',
+                }}
+            />
+        </div>
+    );
+});
+
+const CreateUserInput = ({ callback }: { callback: () => Promise<void> }) => {
+
+    const baseURL = useContext(ApiURLContext);
+
+    const [goneWrong, setGoneWrong] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('Algo deu errado.');
+
+    const nameField = useRef<HTMLInputElement>(null);
+    const emailField = useRef<HTMLInputElement>(null);
+    const passwordField = useRef<HTMLInputElement>(null);
+
+    const validateFields = (): UserCreateFields | undefined => {
+        if (!nameField.current
+            || !emailField.current
+            || !passwordField.current
+            || !nameField.current.value
+            || !emailField.current.value
+            || !passwordField.current.value) {
+            return;
+        }
+
+        const fields: UserCreateFields = {
+            name: nameField.current.value,
+            email: emailField.current.value,
+            password: passwordField.current.value,
+        };
+
+        return fields;
+    }
+
+    const createUser = async () => {
+
+        setGoneWrong(false);
+        setLoading(true);
+
+        const fields = validateFields();
+        if (!fields) return;
+
+        await axios.post(`${baseURL}/users`, {
+            name: fields.name,
+            email: fields.email,
+            password: fields.password,
+        })
+            .then(async () => await callback())
+            .catch((error: AxiosError) => {
+                console.log(error);
+                switch (error.response?.status) {
+                    case 404:
+                        setErrorMessage('Algo deu errado. O link da API está correto?');
+                        break;
+
+                    case 401:
+                        setErrorMessage('Já existe um usuário com este e-mail.');
+                        break;
+
+                    case 400:
+                        setErrorMessage('Veja se os campos estão válidos.');
+                        break;
+                    default:
+                        setErrorMessage('Algo deu errado.');
+                }
+                setGoneWrong(true);
+            });
+
+        setLoading(false);
+    }
+
+    return (
+        <div className='flex center column' style={{ overflow: 'hidden' }}>
+            <p className="text size-125 bold center" style={{ borderBottom: '2px solid var(--element-green)' }}>Criar usuário</p>
+            <div
+                className='flex flex-start column'
+                style={{
+                    backgroundColor: 'var(--dark-background)',
+                    paddingInline: 20,
+                    paddingBlock: 10,
+                    marginInline: 20,
+                    borderRadius: 15,
+                }}
+            >
+                <div>
+                    <InputField ref={nameField}>
+                        Nome:
+                    </InputField>
+                    <InputField ref={emailField}>
+                        E-mail:
+                    </InputField>
+                    <InputField ref={passwordField}>
+                        Senha:
+                    </InputField>
+                </div>
+                {goneWrong && <p className='text error center flex wrap'>{errorMessage}</p>}
+                {loading
+                    ? <LoadingIcon />
+                    : <button
+                        className='create button bold text'
+                        style={{ marginTop: 15, marginBottom: 5 }}
+                        onClick={() => createUser()}
+                    >
+                        Criar
+                    </button>}
+            </div>
+        </div>
+    );
+};
+
 const UserCollection = () => {
 
     const baseURL = useContext(ApiURLContext);
@@ -74,22 +205,29 @@ const UserCollection = () => {
 
     const fetch = async () => {
         setLoading(true);
-        const response = await axios.get('/users', { baseURL });
+        const response = await axios.get('/users', { baseURL })
+            .catch((error) => console.error(error))
+            .then((response) => response ?? { data: { users: [] } });
         setUsers(response.data.users);
         setLoading(false);
     }
 
+    useEffect(() => {
+        fetch()
+    }, []);
+
     const updateButton =
         <button
             className='text center bold size-115 button'
-            style={{ padding: '10px 20px', borderRadius: 12, marginTop: 20 }}
-            onClick={fetch}>
+            style={{ margin: 20 }}
+            onClick={fetch}
+        >
             Atualizar
-        </button >;
+        </button>;
 
     if (loading) {
         return (
-            <div className='flex center column' style={{ gap: 10 }}>
+            <div className='flex center column gap-10'>
                 {updateButton}
                 <LoadingIcon showText />
             </div>
@@ -98,7 +236,8 @@ const UserCollection = () => {
 
     return (
         <div className='flex center column'>
-            <UserTable users={users} />
+            <UserList users={users} />
+            <CreateUserInput callback={fetch} />
             {updateButton}
         </div>
     )
